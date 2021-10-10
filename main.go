5b33462cd60305181775cdac7e02070e9079c5f3
Declan Soper <soperdeclan@gmail.com>
2021-10-10 23:29:41 +0100

    add comments to main.go

--- main.go
+++ main.go
@@ -26,6 +26,9 @@ type binlogDetails struct {
 	Position int64
 }
 
+// eventRecv is a binlog parser callback function
+// events are sent to this function, which then passes
+// relevant events to the binlog executor
 func eventRecv(event *replication.BinlogEvent) error {
 	fmt.Printf(
 		"Executing event (%T) in binlog %s: %d\n",
@@ -35,6 +38,7 @@ func eventRecv(event *replication.BinlogEvent) error {
 	)
 
 	switch t := event.Event.(type) {
+	// if rotate, change current binlog details
 	case *replication.RotateEvent:
 		dir := filepath.Dir(cliFlags.BinlogFile)
 		file := filepath.Join(dir, string(t.NextLogName))
@@ -44,6 +48,7 @@ func eventRecv(event *replication.BinlogEvent) error {
 			Position: int64(t.Position),
 		}
 		return nil
+	// if row event, check for ignored dbs and tables
 	case *replication.RowsEvent:
 		schema, table := string(t.Table.Schema), string(t.Table.Table)
 
@@ -54,12 +59,15 @@ func eventRecv(event *replication.BinlogEvent) error {
 		if containsStrCaseInsensitive(cliFlags.IgnoreTables, schema+"."+table) {
 			return nil
 		}
+	// if GTID event, track the current binlog position
+	// this is used in case of an error
 	case *replication.MariadbGTIDEvent:
 		currentBinlogDetails.Position = int64(event.Header.LogPos)
 		go logPosition("pos.txt", currentBinlogDetails.Position)
 		return nil
 	}
 
+	// execute event
 	if err := executor.ExecuteEvent(event); err != nil {
 		fmt.Printf("%v\n", event.Event)
 		return err
@@ -68,6 +76,7 @@ func eventRecv(event *replication.BinlogEvent) error {
 	return nil
 }
 
+// logPosition writes a given pos to a file at filename
 func logPosition(filename string, pos int64) {
 	f, err := os.Create(filename)
 	if err != nil {
@@ -76,6 +85,8 @@ func logPosition(filename string, pos int64) {
 	f.WriteString(fmt.Sprint(pos))
 }
 
+// buildConnectionString builds a connection string with a username,
+// password, and host
 func buildConnectionString(username, password, host string) string {
 	conf := mysql.NewConfig()
 	conf.User = username
@@ -87,7 +98,9 @@ func buildConnectionString(username, password, host string) string {
 
 func main() {
 	var err error
+	// parse command line args
 	cliFlags = NewCliFlags(os.Args[1:])
+	// open database connection
 	conn, err = sql.Open(
 		"mysql",
 		buildConnectionString(
@@ -101,15 +114,21 @@ func main() {
 	}
 	defer conn.Close()
 
+	// create executor and aim at db via connection
 	executor = NewBinlogExecutor(conn)
+	// create binlog parser
 	parser = replication.NewBinlogParser()
 
+	// initialise current binlog details
 	currentBinlogDetails = binlogDetails{
 		Name:     cliFlags.BinlogFile,
 		Position: cliFlags.Position,
 	}
 
+	// for loop to restart parsing when end of
+	// active binlog is reached
 	for {
+		// start parsing, sends parsed events to eventRecv func
 		err = parser.ParseFile(
 			currentBinlogDetails.Name,
 			currentBinlogDetails.Position,
